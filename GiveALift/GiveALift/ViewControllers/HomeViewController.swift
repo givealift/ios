97e2b9d4fccf0556f426acc68a993244b7d612cc
Marcin Włoczko <mwloczko@gmail.com>
2018-03-23 19:29:05 +0100

    distinguish onboarding views

--- GiveALift/GiveALift/ViewControllers/HomeViewController.swift
+++ GiveALift/GiveALift/ViewControllers/HomeViewController.swift
@@ -8,19 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        view.backgroundColor = .yellow
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
---
+++ GiveALift/GiveALift/ViewControllers/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  GiveALift
+//
+//  Created by Marcin Włoczko on 23.03.2018.
+//  Copyright © 2018 Marcin Włoczko. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+}
