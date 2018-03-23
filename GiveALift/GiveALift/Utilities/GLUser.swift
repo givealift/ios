bbf3ef408a4db29edcbfa72fda181c020c65ff1c
Marcin Włoczko <mwloczko@gmail.com>
2018-03-23 18:09:09 +0100

    add core data

---
+++ GiveALift/GiveALift/Utilities/GLUser.swift
@@ -0,0 +1,14 @@
+//
+//  GLUser.swift
+//  GiveALift
+//
+//  Created by Marcin Włoczko on 23.03.2018.
+//  Copyright © 2018 Marcin Włoczko. All rights reserved.
+//
+
+import Foundation
+
+struct GLUser {
+    var email: String;
+    
+}
