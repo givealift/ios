16387896cc4d04de7121799bd23cf2e098bc2654
Marcin Włoczko <mwloczko@gmail.com>
2018-04-17 21:09:36 +0200

    probably impelemntet textfiled with sugestive citeis

---
+++ GiveALift/GiveALift/Views/SugestiveTextField.swift
@@ -0,0 +1,49 @@
+//
+//  SugestiveTextField.swift
+//  GiveALift
+//
+//  Created by Marcin Włoczko on 17.04.2018.
+//  Copyright © 2018 Marcin Włoczko. All rights reserved.
+//
+
+import UIKit
+import SearchTextField
+
+final class SugestiveTextField: SearchTextField {
+    
+    var cities: [City]!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.cities = loadCities()
+        self.filterStrings(cities.map({$0.name}))
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func selectedCityId() -> Int? {
+        if let cityText = self.text {
+            if let city = cities.first(where: {$0.name == cityText}) {
+                return city.cityID
+            }
+        }
+        return nil
+    }
+    
+    private func loadCities() -> [City] {
+        if let path = Bundle.main.path(forResource: "Cities", ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .dataReadingMapped)
+                let decoder = JSONDecoder()
+                let cities = try decoder.decode([City].self, from: data)
+                return cities
+            } catch {
+                fatalError("Couldn't load cities")
+            }
+        }
+        return []
+    }
+}
